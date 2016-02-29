46366330c02c8d5b79e1c65db163921b5916245d
Jack Franklin <jack@jackfranklin.net>
2016-02-29 16:44:49 +0000

    Prefer concat over push

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function processMakeOptions(options) {
 
     if(options.elmMake) { exe = options.elmMake; }
 
-    if(options.warnings === true) { args.push('--warn'); }
+    if(options.warnings === true) { args = args.concat(['--warn']); }
 
     var ft = options.filetype;
     if(!!ft) {
