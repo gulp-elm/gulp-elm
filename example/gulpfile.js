e96a850de7866d7618951f4369a3ae3ed5c41a5b
Brian Dukes <bdukes@engagesoftware.com>
2018-08-21 14:13:22 -0500

    Update example to gulp 4

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -5,49 +5,56 @@ var fs     = require('fs');
 var Vinyl  = require('vinyl');
 
 // elm-make --yes
-gulp.task('init', elm.init);
-gulp.task('init-nested', function(){
+var init = elm.init;
+function initNested(){
   return elm.init({cwd: 'elm/nested-elm/'});
-});
+}
 
-gulp.task('multi', ['init'], function(){
+function multi(){
   return gulp.src('elm/*.elm')
     .pipe(elm.make({filetype: 'html'}))
     .pipe(gulp.dest('dest/'));
-});
+}
 
-gulp.task('nested', ['init-nested'], function(){
+function nested(){
   return gulp.src('elm/nested-elm/*.elm')
     .pipe(elm.make({filetype: 'html', cwd: 'elm/nested-elm/'}))
     .pipe(gulp.dest('dest/'));
-});
+}
 
-gulp.task('debug', ['init'], function(){
+function debug(){
   return gulp.src('elm/*.elm')
     .pipe(elm.make({filetype: 'html', debug: true }))
     .pipe(gulp.dest('dest/'));
-});
+}
 
-gulp.task('pipe', ['init'], function(){
+function pipe(){
   return gulp.src('elm/test1.elm')
     .pipe(elm())
     .pipe(uglify())
     .pipe(gulp.dest('dest/'));
-});
+}
 
 function string_src(filename, string) {
   var src = require('stream').Readable({ objectMode: true })
   src._read = function () {
-    this.push(new Vinyl({ cwd: "", base: "", path: filename, contents: Buffer.from(string) }))
+    this.push(new Vinyl({ path: filename, contents: Buffer.from(string) }))
     this.push(null)
   }
   return src
 }
 
-gulp.task('string', ['init'], function(){
+function string() {
   return string_src('test3.elm', fs.readFileSync('elm/test1.elm'))
     .pipe(elm())
     .pipe(gulp.dest('dest/'))
-});
+}
 
-gulp.task('default', ['multi', 'pipe', 'string']);
+module.exports = {
+  default: gulp.series(init, gulp.parallel(multi, pipe, string)),
+  pipe: gulp.series(init, pipe),
+  multi: gulp.series(init, multi),
+  string: gulp.series(init, string),
+  debug: gulp.series(init, debug),
+  nested: gulp.series(initNested, nested),
+};
