aec5f3abbed25238b4d0718518e1a864e8f13a7b
Brian Dukes <bdukes@engagesoftware.com>
2019-10-22 12:08:50 -0500

    Add Uglify optimization in example

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -26,10 +26,45 @@ function debug() {
 }
 
 function pipe() {
+  // Uglify parameters based on  https://elm-lang.org/0.19.1/optimize
   return gulp
     .src("elm/test1.elm")
-    .pipe(elm())
-    .pipe(uglify())
+    .pipe(elm({ optimize: true }))
+    .pipe(
+      uglify({
+        compress: {
+          pure_funcs: [
+            "F2",
+            "F3",
+            "F4",
+            "F5",
+            "F6",
+            "F7",
+            "F8",
+            "F9",
+            "A2",
+            "A3",
+            "A4",
+            "A5",
+            "A6",
+            "A7",
+            "A8",
+            "A9"
+          ],
+          pure_getters: true,
+          keep_fargs: false,
+          unsafe_comps: true,
+          unsafe: true
+        },
+        mangle: false
+      })
+    )
+    .pipe(
+      uglify({
+        compress: false,
+        mangle: true
+      })
+    )
     .pipe(gulp.dest("dest/"));
 }
 
